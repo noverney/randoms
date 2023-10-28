cc3030341af3ad51d07af8eb6e0be774e4a30c83
Allard <AllardQuek@users.noreply.github.com>
2023-10-29 00:09:31 +0200

    Fix merge conflict

--- frontend/pages/user.vue
+++ frontend/pages/user.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-screen">
-    <UCard class="w-auto sm:min-w-80 max-w-md" :ui="{ shadow: 'shadow-lg' }">
-      <div v-if="isLoading" class="flex items-center space-x-4">
-        <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
-        <div class="space-y-2">
-          <USkeleton class="h-4 w-[250px]" />
-          <USkeleton class="h-4 w-[200px]" />
-        </div>
-      </div>
-
-      <div v-else class="flex flex-col items-center space-x-4">
+    <UCard class="w-9/12 sm:min-w-80 max-w-md" :ui="{ shadow: 'shadow-lg' }">
+      <div class="flex flex-col items-center space-x-4">
         <div class="flex items-center space-x-4 pb-4">
           <UAvatar
             src="https://avatars.githubusercontent.com/u/25683183?v=4"
@@ -21,38 +13,62 @@
             <p>RDIA</p>
           </div>
         </div>
-        <!-- nuxt UI text component for funfacts -->
         <UTextarea
+          class="w-9/12"
           autoresize
           v-model="funfacts"
           :ui="{ placeholder: 'Fun facts' }"
         />
-        <div class="pt-3 space-x-2">
-          <UBadge color="sky" variant="outline">Mangas</UBadge>
-          <UBadge color="sky" variant="outline">Harry Potter</UBadge>
-          <UBadge color="sky" variant="outline">Hacking</UBadge>
-          <UButton
-            icon="i-heroicons-pencil-square"
-            size="2xs"
-            color="primary"
-            square
-            variant="outline"
+        <div class="py-3 space-x-2">
+          <USelectMenu
+            v-model="selected"
+            :options="interests"
+            multiple
+            placeholder="Select interests"
           />
         </div>
+        <UButton color="primary" variant="solid" @click="saveChanges"
+          >Save Changes</UButton
+        >
       </div>
     </UCard>
   </div>
 </template>
 
 <script setup lang="ts">
+import { doc, setDoc } from "firebase/firestore";
+
 definePageMeta({
   middleware: ["auth"],
 });
 const isLoading = ref(false);
+const interests = ["Mangas", "Harry Potter", "Hacking"];
+const selected = ref([]);
 
 const username = ref("Joe Doe");
 const userMatched = ref("Joe Doe");
 const funfacts = ref(
   "I have a talent for remembering an unusual number of random trivia facts. It's like having a mental library of quirky information, and I'm always ready to share a fun fact at any social gathering!"
 );
+
+const firestore = useFirestore();
+const user = useCurrentUser();
+
+const saveChanges = async () => {
+  // Save the interests and funfacts to the users database
+  console.log("SAVING");
+  if (!user.value?.uid) {
+    console.log("No user");
+    return;
+  }
+
+  const interestsObject = Object.fromEntries(selected.value.map((x) => [x, 5]));
+  console.log(interestsObject);
+
+  await setDoc(doc(firestore, "users", user.value.uid), {
+    preferences: interestsObject,
+    funfacts: funfacts.value,
+  });
+  console.log("SAVED");
+};
 </script>
