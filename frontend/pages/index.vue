2ce0e3ef301c5e737c424e3383abc202f2cda5cf
Allard <AllardQuek@users.noreply.github.com>
2023-10-28 20:52:05 +0200

    Add index and user screens

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,37 +1,65 @@
 <template>
-    <div>
-        <h1>
-            hello from firebase hosting - new one
-        </h1>
-        <NuxtLink to="/login">Login</NuxtLink>
-        <NuxtLink to="/user">User</NuxtLink>
+  <!-- <NuxtLink to="/login">Login</NuxtLink>
+  <NuxtLink to="/user">User</NuxtLink>
+  <br />
 
-        <br>
+  <button @click="logout">LogOut</button> -->
 
-        <button @click="logout">LogOut</button>
+  <!-- <pre>User: {{ user?.displayName }}</pre>
 
-        <pre>User: {{ user?.displayName }}</pre>
+  <pre>{{ users }}</pre> -->
+  <div class="flex items-center justify-center h-screen">
+    <UCard class="w-auto min-w-80 max-w-md" :ui="{ shadow: 'shadow-lg' }">
+      <div v-if="isLoading" class="flex items-center space-x-4">
+        <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
+        <div class="space-y-2">
+          <USkeleton class="h-4 w-[250px]" />
+          <USkeleton class="h-4 w-[200px]" />
+        </div>
+      </div>
 
-        <pre>{{ users }}</pre>
-    </div>
+      <div v-else class="flex flex-col items-center space-x-4">
+        <div class="flex items-center space-x-4">
+          <UAvatar
+            src="https://avatars.githubusercontent.com/u/25683183?v=4"
+            alt="Avatar"
+            size="3xl"
+          />
+          <div class="space-y-2">
+            <p class="font-semibold text-xl">{{ users.at(0)?.name }}</p>
+            <p>RDIA</p>
+          </div>
+        </div>
+        <p class="italic pt-3">"{{ funfacts }}"</p>
+        <div class="pt-3 space-x-2">
+          <UBadge color="sky" variant="outline">Mangas</UBadge>
+          <UBadge color="sky" variant="outline">Harry Potter</UBadge>
+          <UBadge color="sky" variant="outline">Hacking</UBadge>
+        </div>
+      </div>
+    </UCard>
+  </div>
 </template>
 
-
 <script setup lang="ts">
-import { collection } from 'firebase/firestore';
+import { collection } from "firebase/firestore";
 
 definePageMeta({
-    middleware: ['auth'],
-})
+  middleware: ["auth"],
+});
 
-const firestore = useFirestore()
-const auth = useFirebaseAuth()!
-const user = useCurrentUser()
-useFirestore()
+const funfacts = ref(
+  "I have a talent for remembering an unusual number of random trivia facts. It's like having a mental library of quirky information, and I'm always ready to share a fun fact at any social gathering!"
+);
 
-const users = useCollection(collection(firestore, 'users'))
+const firestore = useFirestore();
+const auth = useFirebaseAuth()!;
+const user = useCurrentUser();
+useFirestore();
+
+const users = useCollection(collection(firestore, "users"));
 
 const logout = async () => {
-    await auth.signOut()
-}
+  await auth.signOut();
+};
 </script>
--- frontend/pages/user.vue
+++ frontend/pages/user.vue
@@ -1,9 +1,58 @@
 <template>
-    <div>Hello from user settings page</div>
+  <div class="flex items-center justify-center h-screen">
+    <UCard class="w-auto sm:min-w-80 max-w-md" :ui="{ shadow: 'shadow-lg' }">
+      <div v-if="isLoading" class="flex items-center space-x-4">
+        <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
+        <div class="space-y-2">
+          <USkeleton class="h-4 w-[250px]" />
+          <USkeleton class="h-4 w-[200px]" />
+        </div>
+      </div>
+
+      <div v-else class="flex flex-col items-center space-x-4">
+        <div class="flex items-center space-x-4 pb-4">
+          <UAvatar
+            src="https://avatars.githubusercontent.com/u/25683183?v=4"
+            alt="Avatar"
+            size="3xl"
+          />
+          <div class="space-y-2">
+            <p class="font-semibold text-xl">{{ username }}</p>
+            <p>RDIA</p>
+          </div>
+        </div>
+        <!-- nuxt UI text component for funfacts -->
+        <UTextarea
+          autoresize
+          v-model="funfacts"
+          :ui="{ placeholder: 'Fun facts' }"
+        />
+        <div class="pt-3 space-x-2">
+          <UBadge color="sky" variant="outline">Mangas</UBadge>
+          <UBadge color="sky" variant="outline">Harry Potter</UBadge>
+          <UBadge color="sky" variant="outline">Hacking</UBadge>
+          <UButton
+            icon="i-heroicons-pencil-square"
+            size="2xs"
+            color="primary"
+            square
+            variant="outline"
+          />
+        </div>
+      </div>
+    </UCard>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-    middleware: ['auth'],
-})
+  middleware: ["auth"],
+});
+const isLoading = ref(false);
+
+const username = ref("Joe Doe");
+const userMatched = ref("Joe Doe");
+const funfacts = ref(
+  "I have a talent for remembering an unusual number of random trivia facts. It's like having a mental library of quirky information, and I'm always ready to share a fun fact at any social gathering!"
+);
 </script>
