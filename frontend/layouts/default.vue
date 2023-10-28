5031f0235d754e58a8866285b549794ef6129393
Tobias Blum <tobiasblum02@gmail.com>
2023-10-28 21:15:26 +0200

    Fix vue inject error

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,27 +1,16 @@
 <template>
   <header class="shrink-0 bg-gray-900">
-    <div
-      class="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8"
-    >
+    <div class="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
       <NuxtLink to="/">
-        <img
-          class="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-          alt="Your Company"
-        />
+        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+          alt="Your Company" />
       </NuxtLink>
       <div class="flex items-center gap-x-8">
-        <UDropdown
-          :items="items"
-          :ui="{ item: { disabled: 'cursor-text select-text' } }"
-          :popper="{ placement: 'bottom-start' }"
-        >
-          <UAvatar
-            :src="
-              currentUser?.photoURL ||
-              'https://avatars.githubusercontent.com/u/739984?v=4'
-            "
-          />
+        <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }"
+          :popper="{ placement: 'bottom-start' }">
+          <UAvatar :src="currentUser?.photoURL ||
+            'https://avatars.githubusercontent.com/u/739984?v=4'
+            " />
           <template #account="{ item }">
             <div class="text-left">
               <p>Signed in as</p>
@@ -32,10 +21,7 @@
           </template>
           <template #item="{ item }">
             <span class="truncate">{{ item.label }}</span>
-            <UIcon
-              :name="item.icon"
-              class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto"
-            />
+            <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
           </template>
         </UDropdown>
       </div>
@@ -49,8 +35,11 @@
 </template>
 
 <script setup lang="ts">
+import { collection, doc, setDoc } from 'firebase/firestore';
+
 const currentUser = useCurrentUser();
 const auth = useFirebaseAuth();
+const firestore = useFirestore()
 
 const items = [
   [
@@ -79,4 +68,22 @@ const items = [
     },
   ],
 ];
+
+// redirect user on first sign up and init user document
+const user = await getCurrentUser()
+const me = useDocument(doc(collection(firestore, 'users'), user.uid))
+
+watch([me], async () => {
+  if (user && !me.value) {
+    console.log("doc created")
+    console.log(user, me.value)
+    await setDoc(doc(firestore, "users", user.uid), {
+      name: user.displayName,
+      preferences: {},
+      days: [],
+    });
+    navigateTo('/user')
+  }
+})
+
 </script>
