585b9d725164528a6d01f0e959b9ba6b94b21c50
BAAAKA <baaakaDev@protonmail.ch>
2023-10-28 17:52:05 +0200

    switched name to id

--- functions/matching_users.py
+++ functions/matching_users.py
@@ -6,8 +6,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import collections
 
 def get_sorted_list_func(users, user_preferences):
-    def get_sorted_list(user_id):
-        user_score_list = {users[partner_id]:score for partner_id, score in enumerate(user_preferences[user_id]) if partner_id != user_id}
+    def get_sorted_list(user_index):
+        user_score_list = {users[partner_id]:score for partner_id, score in enumerate(user_preferences[user_index]) if partner_id != user_index}
         user_score_ordered = collections.OrderedDict(sorted(user_score_list.items(), key=lambda item: item[1], reverse=True))
         return list(user_score_ordered)
     return get_sorted_list
@@ -18,17 +18,20 @@ def check_input(users, preference_matrix):
         raise ValueError(f"The user limit is {user_limit}")
     if len(users) != len(preference_matrix):
         raise ValueError(f"user list and matrix length do not match")
-    if len(users) != len(set(users)):
-        raise ValueError(f"Duplicates exist in the users array")
-
 
 def get_preference_lists(users, preference_matrix):
     check_input(users, preference_matrix)
 
     user_preferences = cosine_similarity(preference_matrix)
     get_sorted_list = get_sorted_list_func(users, user_preferences)
-    return [get_sorted_list(user_id) for user_id in range(len(users))]
+    return [get_sorted_list(user_index) for user_index in range(len(users))]
 
+
+def match_users(users):
+
+
+    matches = [("You", "A Penguin")]
+    return matches
 
 
 
