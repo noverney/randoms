2e1ca42f39ea21b856e5b4d3f7d67b16481de342
Alex Rovner <rovnero@gmail.com>
2023-10-28 15:50:29 +0200

    add dummy matching trigger endpoint

--- functions/main.py
+++ functions/main.py
@@ -4,9 +4,30 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from firebase_admin import firestore
 
-initialize_app()
+default_app = initialize_app()
+
+class User:
+  def __init__(self, id: str, doc: dict):
+    self.id = id
+    self.name = doc["name"]
+    self.preferences = doc["preferences"]
+  
+  def __str__(self):
+    return f"User({self.id}, {self.name}, {self.preferences})"
+
+def get_all_users():
+  db = firestore.client()
+  docs = (
+    db.collection("users")
+    .stream())
+  return [ User(doc.id, doc.to_dict()) for doc in docs ]
+
+def match_users(users: list[User]):
+  # this is the entrypoint for your matching code
+  return zip(users[0::2], users[1::2])
 
 @https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+def trigger_matching(req: https_fn.Request) -> https_fn.Response:
+  return https_fn.Response(",".join([f"{str(x[0])}+{str(x[1])}" for x in match_users(get_all_users())]))
